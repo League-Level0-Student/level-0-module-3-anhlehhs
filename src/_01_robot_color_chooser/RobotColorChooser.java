cef7aea2d62c5e5267904e2ec202d6b5d6c3bff5
root <root@1dd06423a2ed>
2020-09-23 22:29:42 +0000

    robot color chooser

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,44 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot john = new Robot();
+		// 3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 10; i++) {
 
-        //6. If the user doesn’t enter anything, choose a random color
+			String input = JOptionPane.showInputDialog(null, "What is your favourite color?");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (input.equalsIgnoreCase("blue")) {
+				john.setPenColor(Color.BLUE);
+			} else if (input.equalsIgnoreCase("red")) {
+				john.setPenColor(Color.RED);
+			} else if (input.equalsIgnoreCase("green")) {
+				john.setPenColor(Color.GREEN);
+			} else if (input.equalsIgnoreCase("yellow")) {
+				john.setPenColor(Color.YELLOW);
+			} else {
+				john.setRandomPenColor();
+			}
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			// 6. If the user doesn’t enter anything, choose a random color
 
-
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
+			// 4. Set the pen width to 10
+			john.setPenWidth(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			john.penDown();
+			for (int j = 0; i < 4; j++) {
+				john.move(100);
+				john.turn(90);
+			}
+		}
 	}
 }
--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -11,9 +11,9 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "September 9th";
+		String dadsBirthday = "September 19th";
+		String myBirthday = "April 9th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
 	

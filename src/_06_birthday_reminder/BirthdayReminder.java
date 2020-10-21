e69bdeba6d4591a78346e185af404b7e23cb9a49
root <root@9699a64f5c40>
2020-10-21 22:02:48 +0000

    yes

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -16,9 +16,15 @@ public class BirthdayReminder {
 		String myBirthday = "April 9th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String input=JOptionPane.showInputDialog("what birthday do you want?");
 		// 3. Print out what the user typed
-		
+		if(input.equalsIgnoreCase("mom")) {
+		System.out.println(momsBirthday);
+		}else if(input.equalsIgnoreCase("dad")) {
+			System.out.println(dadsBirthday);
+		}else if(input.equalsIgnoreCase("your birthday")) {
+			System.out.println(myBirthday);
+			
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
@@ -27,5 +33,8 @@ public class BirthdayReminder {
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
-	} 
+	}else {
+		System.out.println("Sorry I dont remember that person's birthday!");
+	}
+}
 }
